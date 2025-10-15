5d37f4969adde72e4c5c6bd0357ac9cd3a403d44
Calum Eadie <199819990+ce10-sanger@users.noreply.github.com>
2025-10-15 17:10:53 +0100

    Experimenting with predicates

--- src/npg_irods/functions.py
+++ src/npg_irods/functions.py
@@ -21,11 +21,32 @@ other categories (such as the instrument-specific modules or metadata)."""
 
 import re
 from pathlib import Path
+from typing import Callable, TypeVar
 
 from structlog.stdlib import get_logger
 
 log = get_logger(__name__)
 
+T = TypeVar("T")
+Predicate = Callable[[T], bool]
+PathPredicate = Predicate[Path]
+
+def is_true(_: Path) -> bool:
+    """Always returns true."""
+    return True
+
+def is_false(_: Path) -> bool:
+    """Always returns false."""
+    return False
+
+# def and(*preds: Predicate[T]) -> Predicate[T]:
+#     return lambda x: all(p(x) for p in preds)
+#
+# def Or(*preds: Predicate[T]) -> Predicate[T]:
+#     return lambda x: any(p(x) for p in preds)
+
+# def Not(pred: Predicate[T]) -> Predicate[T]:
+#     return lambda x: not pred(x)
 
 def _is_top_level_file(path: Path, parent: Path):
     if not path.is_file():
@@ -36,6 +57,23 @@ def _is_top_level_file(path: Path, parent: Path):
 
     return path_absolute.parent == parent_absolute
 
+def _make_match_pattern(pattern: str, flags: int | re.RegexFlag = 0) -> PathPredicate:
+    r = re.compile(pattern, flags=flags)
+    def f(path: Path) -> bool:
+        return bool(r.search(path.as_posix()))
+    return f
+
+def _make_match_patterns(patterns: list[str], flags: int | re.RegexFlag = 0) -> PathPredicate:
+    regexes = [re.compile(p, flags=flags) for p in patterns]
+    def f(path: Path) -> bool:
+        return any(r.search(path.as_posix()) for r in regexes)
+    return f
+
+def match_patterns(path: Path, patterns: list[str], flags: int | re.RegexFlag = 0) -> bool:
+    regexes = [re.compile(p, flags=flags) for p in patterns]
+    return any(r.search(path.as_posix()) for r in regexes)
+
+is_md5 = _make_match_pattern(".md5")
 
 def make_path_filter(
     include_patterns: list[str] = None,
@@ -44,7 +82,7 @@ def make_path_filter(
     exclude_md5: bool = False,
     src: Path | None = None,
     flags: int | re.RegexFlag = 0,
-):
+) -> PathPredicate:
     """Return a function that filters paths based on the given filters.
 
     Args:
@@ -67,43 +105,31 @@ def make_path_filter(
     include_patterns = include_patterns or []
     exclude_patterns = exclude_patterns or []
 
-    include_regexes = [re.compile(p, flags=flags) for p in include_patterns]
-    exclude_regexes = [re.compile(p, flags=flags) for p in exclude_patterns]
-
-    if exclude_md5:
-        exclude_regexes.append(re.compile(".md5"))
+    # include_regexes = [re.compile(p, flags=flags) for p in include_patterns]
+    # exclude_regexes = [re.compile(p, flags=flags) for p in exclude_patterns]
+    #
+    # match_include_patterns = _make_match_patterns(include_patterns, flags) if include_patterns else is_true
+    # match_exclude_patterns = _make_match_patterns(exclude_patterns, flags) if exclude_patterns else is_true
 
     def path_filter(path: Path) -> bool:
-        if include_regexes or include_top_level_files:
+        if include_patterns or include_top_level_files:
             include = False
 
-            for r in include_regexes:
-                if r.search(path.as_posix()):
-                    log.debug(
-                        "Filtering path",
-                        path=path,
-                        matched=r,
-                        include_regexes=include_regexes,
-                    )
-                    include = True
-                    break
+            if match_patterns(path, include_patterns, flags):
+                include = True
 
             if include_top_level_files:
                 if _is_top_level_file(path, src):
-                    log.debug("Filtering path", path=path, is_top_level_file=True)
                     include = True
 
             if not include:
                 return True
 
-        for r in exclude_regexes:
-            if r.search(path.as_posix()):
-                log.debug(
-                    "Filtering path",
-                    path=path,
-                    matched=r,
-                    exclude_regexes=exclude_regexes,
-                )
+        if match_patterns(path, exclude_patterns, flags):
+            return True
+
+        if exclude_md5:
+            if is_md5(path):
                 return True
 
         return False
