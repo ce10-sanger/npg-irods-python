cc6d6c528491b5da9d9527eab5226a8dad40e2b2
Calum Eadie <199819990+ce10-sanger@users.noreply.github.com>
2026-07-14 11:34:25 +0100

    Rough implementation of reader that supports streaming

--- src/npg_irods/utilities.py
+++ src/npg_irods/utilities.py
@@ -1260,7 +1260,47 @@ def read_md5_file(path: Path) -> str:
             raise ValueError(f"MD5 checksum is not 32 characters: '{md5}'")
         return md5
 
+class Md5sumsReader:
 
+    def __init__(self, md5sums_path: Path):
+        self._md5sums_path = md5sums_path
+        self._md5sums = {}
+        self._md5sums_modified = md5sums_path.stat().st_mtime
+        # TODO: Resource handling
+        self._md5sums_file = self._md5sums_path.open()
+
+        self._read_new_lines()
+
+    def get_md5sum(self, path: Path | str) -> str:
+        path = Path(path) if isinstance(path, str) else path
+        path = path.resolve()
+        checksum = self._md5sums.get(path)
+        if not checksum:
+            self._read_new_lines()
+        checksum = self._md5sums.get(path)
+        if not checksum:
+            raise ValueError(f"No checksum found for {path}")
+        path_modified = path.stat().st_mtime
+        if path_modified > self._md5sums_modified:
+            raise ValueError(
+                f"Checksum for {path} may be out of date, file modified ({path_modified}) more recently than {self._md5sums_path} ({self._md5sums_modified})"
+            )
+        return checksum
+
+    def _read_new_lines(self):
+        # TODO: Logging
+
+        self._md5sums_modified = self._md5sums_path.stat().st_mtime
+
+        while line := self._md5sums_file.readline():
+            line = line.strip()
+            md5, path = line.split("  ", 1)
+            if len(md5) != 32:
+                raise ValueError(
+                    f"MD5 checksum is not 32 characters: '{md5}'")
+            self._md5sums[Path(path)] = md5
+
+# TODO: Refactor
 def read_md5sums_file(path: Path) -> dict[Path, str]:
     """
     Reads an MD5 checksums file produced by checksum-directory or another tool
@@ -1317,20 +1357,9 @@ def sanitise_path(path: str | None) -> str | None:
 
 
 def make_get_checksum(md5sums_path: Path) -> Callable[[Path | str], str]:
-    md5sums = read_md5sums_file(md5sums_path)
-    md5sums_modified = md5sums_path.stat().st_mtime
+    md5sums_reader = Md5sumsReader(md5sums_path)
 
     def get_checksum(path: Path | str) -> str:
-        path = Path(path) if isinstance(path, str) else path
-        path = path.resolve()
-        checksum = md5sums.get(path)
-        if not checksum:
-            raise ValueError(f"No checksum found for {path}")
-        path_modified = path.stat().st_mtime
-        if path_modified > md5sums_modified:
-            raise ValueError(
-                f"Checksum for {path} may be out of date, file modified ({path_modified}) more recently than {md5sums_path} ({md5sums_modified})"
-            )
-        return checksum
+        return md5sums_reader.get_md5sum(path)
 
     return get_checksum
