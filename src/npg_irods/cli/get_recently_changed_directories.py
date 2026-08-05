cb349f65537edfbe7d2149fba77cf868808c7083
Calum Eadie <199819990+ce10-sanger@users.noreply.github.com>
2026-08-05 15:25:17 +0100

    Copyright

--- src/npg_irods/cli/get_recently_changed_directories.py
+++ src/npg_irods/cli/get_recently_changed_directories.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2026 Genome Research Ltd. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @author Calum Eadie <ce10@sanger.ac.uk>
 from datetime import datetime, timedelta
 
 import operator
@@ -14,7 +31,6 @@ from npg.log import configure_structlog
 from npg_irods import add_appinfo_structlog_processor, version
 from npg_irods.utilities import sanitise_path
 
-# TODO: Copyright
 # TODO: Docs
 # TODO: Where should this live?
 # TODO: Expected runtime
